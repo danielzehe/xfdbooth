d7a1c494e9755cae909b5261f2b92445f823fd11
Daniel Zehe <daniel.zehe@tum-create.edu.sg>
2017-03-25 21:35:41 +0100

    Modified: -make fullscreen only on arm and flip the video image

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const electron = require('electron')
+const os = require('os');
 // Module to control application life.
 const app = electron.app
 // Module to create native browser window.
@@ -13,7 +14,12 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 480,frame:false})
+
+  mainWindow = new BrowserWindow({width: 800, height: 480,frame:false,resizable:false})
+  if(os.arch()=='arm'){
+    mainWindow.setFullscreen(true);
+  }
+
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
