fa68f0562395a63e4a67d9cad156400c012f5270
Daniel Zehe <daniel.zehe@tum-create.edu.sg>
2017-03-25 20:23:56 +0100

    Initial commit

---
+++ renderer.js
@@ -0,0 +1,11 @@
+navigator.webkitGetUserMedia({video: true},
+  function(stream) {
+    document.getElementById('camera').src = URL.createObjectURL(stream);
+  },
+  function() {
+    alert('could not connect stream');
+  }
+);
+function takepic(){
+	console.log("hallo");
+}
